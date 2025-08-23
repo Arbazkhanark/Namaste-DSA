9f98bce07e7943d2140a963a756a2f1166b92ed1
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-08-23 14:28:05 +0530

    Time: 16 ms (10.79%), Space: 64.2 MB (60.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,12 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let res="";
     let original=x;
+    let rim;
+    let rev=0;
     while(x>0){
-        res += x%10;
+        rim= x%10;
+        rev= rev*10+rim;
         x = Math.floor(x/10);
     }
 
-    return res == original;
+    return rev == original;
 };
