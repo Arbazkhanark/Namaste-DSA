d2066ff938775005b19e363d8dd4604ed2d1cc95
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-10-22 14:24:12 +0530

    Time: 0 ms (100%), Space: 57.7 MB (40.55%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -9,16 +9,26 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-    let prev=null;
-    let curr=head;
+var reverseList = function (head) {
+    // 'prev' pehle null hai kyunki reverse list ka pehla node ka next null hoga
+    let prev = null;
 
-    while(curr!==null){
-        let temp=curr.next;
-        curr.next=prev;
-        prev=curr;
-        curr=temp;
+    // 'curr' current node ko point karega jisko hum process kar rahe hain
+    let curr = head;
+
+    // Jab tak list khatam nahi ho jaati (curr null nahi hota)
+    while (curr !== null) {
+        // Next node ko temporarily store kar lete hain
+        let temp = curr.next;
+
+        // Current node ka pointer reverse karte hain
+        curr.next = prev;
+
+        // prev aur curr ko ek step aage le jaate hain
+        prev = curr;
+        curr = temp;
     }
-    head=prev;
-    return head;
+
+    // Jab loop khatam hota hai, 'prev' naya head hota hai reversed list ka
+    return prev;
 };
