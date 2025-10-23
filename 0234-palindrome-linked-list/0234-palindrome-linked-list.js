d4180c8726b7cd734db0f59a399d1639ddab2b8a
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-10-23 18:07:26 +0530

    Time: 3 ms (94.61%), Space: 77 MB (63.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,19 +10,54 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-    let curr = head;
+    // let curr = head;
 
-    let arr = [];
-    while (curr) {
-        arr.push(curr.val);
-        curr = curr.next;
+    // let arr = [];
+    // while (curr) {
+    //     arr.push(curr.val);
+    //     curr = curr.next;
+    // }
+
+    // let arrLength = arr.length - 1
+    // for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    //     if (arr[i] !== arr[arrLength - i]) {
+    //         return false;
+    //     }
+    // }
+
+    // return true;
+
+
+
+
+
+    let slow=head;
+    let fast=head;
+
+    while(fast && fast.next!==null){
+        slow=slow.next;
+        fast=fast.next.next;
     }
 
-    let arrLength = arr.length - 1
-    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-        if (arr[i] !== arr[arrLength - i]) {
+
+    let prev=null;
+    let curr=slow;
+
+    while(curr){
+        let temp=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=temp;
+    }
+
+    let start=head;
+    while(prev && start){
+        if(start.val !== prev.val){
             return false;
         }
+
+        prev=prev.next;
+        start=start.next;
     }
 
     return true;
