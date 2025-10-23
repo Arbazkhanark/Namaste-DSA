c76c3f7c5683dc3ec6e5a3f27eb64b2877cebb10
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-10-23 12:34:35 +0530

    Time: 20 ms (23.38%), Space: 84 MB (28.86%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -9,20 +9,18 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    let curr=head;
+var isPalindrome = function (head) {
+    let curr = head;
 
-    let arr=[];
-    while(curr){
+    let arr = [];
+    while (curr) {
         arr.push(curr.val);
-        curr=curr.next;
+        curr = curr.next;
     }
 
-    let arrLength=arr.length-1
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]==arr[arrLength-i]){
-            continue;
-        }else{
+    let arrLength = arr.length - 1
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+        if (arr[i] !== arr[arrLength - i]) {
             return false;
         }
     }
