b9a88c849eab59bdbe0fd94a9abb4c193d34dd7c
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-10-24 13:57:15 +0530

    Time: 543 ms (5.01%), Space: 62.1 MB (88.35%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    
+    let currB=headB;
+
+    while(headA !==null ){
+        let currA=headA;
+
+        for(let currB=headB;currB!==null;currB=currB.next){
+            if(currA==currB){
+                return currA;
+            }
+        }
+        headA=headA.next;
+    }
+    return null;
+};
