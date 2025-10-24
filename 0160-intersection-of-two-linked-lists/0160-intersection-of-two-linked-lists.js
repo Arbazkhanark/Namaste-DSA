0f715cd0682769251a724b09698fb598c9d228ec
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-10-24 14:24:00 +0530

    Time: 71 ms (20.26%), Space: 65.8 MB (16.73%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -11,16 +11,46 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-var getIntersectionNode = function(headA, headB) {
-    while(headA !==null ){
-        let currA=headA;
+var getIntersectionNode = function (headA, headB) {
+    // while(headA !==null ){
+    //     let currA=headA;
 
-        for(let currB=headB;currB!==null;currB=currB.next){
-            if(currA==currB){
-                return currA;
-            }
-        }
-        headA=headA.next;
+    //     for(let currB=headB;currB!==null;currB=currB.next){
+    //         if(currA==currB){
+    //             return currA;
+    //         }
+    //     }
+    //     headA=headA.next;
+    // }
+    // return null;
+
+
+
+
+
+
+
+
+    // Second Approach with better time complexity
+
+
+
+    const visited = new Set();
+
+    // Store all nodes of list B
+    while (headB) {
+        visited.add(headB);
+        headB = headB.next;
     }
+
+    // Check which node in list A exists in set
+    while (headA) {
+        if (visited.has(headA)) {
+            return headA; // Intersection found
+        }
+        headA = headA.next;
+    }
+
+    // No intersection
     return null;
 };
