05f024a792e134f8fd4cea76391965d68ecf371c
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-11-01 23:47:42 +0530

    Time: 0 ms (100%), Space: 54.2 MB (94.1%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -13,27 +13,53 @@
 
 var removeNthFromEnd = function(head, n) {
 
-    let sentinel = new ListNode(0);
-    sentinel.next = head;
+    // One Pass Solution
+    
+    // let sentinel = new ListNode(0);
+    // sentinel.next = head;
 
-    let size = 0;
-    let curr = head;
+    // let size = 0;
+    // let curr = head;
 
-    // count size
-    while (curr !== null) {
-        size++;
-        curr = curr.next;
+    // // count size
+    // while (curr !== null) {
+    //     size++;
+    //     curr = curr.next;
+    // }
+
+    // let targetIndex = size - n+1;  // index of node to delete (0-based)
+    // curr = sentinel; // ✅ start from sentinel, not head
+
+    // // move to previous node of target
+    // for (let i = 1; i < targetIndex; i++) {
+    //     curr = curr.next;
+    // }
+
+    // // delete node
+    // curr.next = curr.next.next;
+    // return sentinel.next;
+
+
+
+
+    // One Pass 
+    let sentinelNode = new ListNode(0, head);
+    let slow = sentinelNode;
+    let fast = head;
+
+    // move fast n steps
+    for (let i = 0; i < n; i++) {
+        fast = fast.next;
     }
 
-    let targetIndex = size - n+1;  // index of node to delete (0-based)
-    curr = sentinel; // ✅ start from sentinel, not head
-
-    // move to previous node of target
-    for (let i = 1; i < targetIndex; i++) {
-        curr = curr.next;
+    // move both pointers
+    while (fast !== null) {
+        fast = fast.next;
+        slow = slow.next;
     }
 
-    // delete node
-    curr.next = curr.next.next;
-    return sentinel.next;
+    // delete the node
+    slow.next = slow.next.next;
+
+    return sentinelNode.next;
 };
