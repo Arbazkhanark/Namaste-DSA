8194e3b1342012a9a764d59c80e89b7275ef2cb9
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-11-03 11:13:10 +0530

    Time: 0 ms (100%), Space: 55.2 MB (13.16%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -10,18 +10,35 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var preorderTraversal = function(root) {
+var preorderTraversal = function(root) { // Root->Left->Right
 
-    let ans=[];
+    // let ans=[];
+    // let stack=new Stack();
 
-    function traverse(curr){
-        if(curr==null) return null;
-        ans.push(curr.val);
-        traverse(curr.left);
-        traverse(curr.right);
-    }
+    // function traverse(curr){
+    //     if(curr==null) return null;
+    //     ans.push(curr.val);
+    //     traverse(curr.left);
+    //     traverse(curr.right);
+    // }
     
-    traverse(root)
+    // traverse(root)
+
+    // return ans;
+
+
+
+//------- Iterative Approach using Stack -------//
+    if(!root) return [];
+    let ans=[];
+    let stack=[root];
+
+    while(stack.length){
+        let curr=stack.pop();
+        ans.push(curr?.val);
+        curr?.right && stack.push(curr.right);
+        curr?.left && stack.push(curr?.left);
+    }
 
     return ans;
 };
