e62a2058e838ce1678f5dd9a05c343c5c54305af
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-11-05 03:14:21 +0530

    Time: 0 ms (100%), Space: 54.2 MB (67.32%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -10,17 +10,47 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var postorderTraversal = function(root) {
+
+
+
+ function postOrderTraversalUsingItrative(root){
+    if(!root) return [];
+
+    let stack1=[root];
+    let stack2=[];
+    while(stack1.length){
+        let curr=stack1.pop();
+        stack2.push(curr);
+        curr.left && stack1.push(curr.left);
+        curr.right && stack1.push(curr.right);
+    }
+
     let ans=[];
 
+    while(stack2.length){
+        ans.push(stack2.pop().val);
+    }
+
+    return ans
+ }
+
+
+var postorderTraversal = function(root) {
+    let res= postOrderTraversalUsingItrative(root);
+    // let res= postOrderTraversalUsingRecursion(root);
+    return res;
+};
+
+
+
+function postOrderTraversalUsingRecursion(root){
+    let ans=[];
     function traverse(curr){
         if(!curr) return;
         traverse(curr.left)
         traverse(curr.right);
         ans.push(curr.val)
     }
-
     traverse(root);
-
     return ans;
-};
+}
