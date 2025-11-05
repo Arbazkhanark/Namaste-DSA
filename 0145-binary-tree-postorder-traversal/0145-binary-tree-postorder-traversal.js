7f20af3196fb5e6ad63ad3d530165c33f4f6303a
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2025-11-06 04:41:13 +0530

    Time: 0 ms (100%), Space: 54.6 MB (33.26%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -13,44 +13,82 @@
 
 
 
- function postOrderTraversalUsingItrative(root){
-    if(!root) return [];
-
-    let stack1=[root];
-    let stack2=[];
-    while(stack1.length){
-        let curr=stack1.pop();
-        stack2.push(curr);
-        curr.left && stack1.push(curr.left);
-        curr.right && stack1.push(curr.right);
-    }
-
-    let ans=[];
-
-    while(stack2.length){
-        ans.push(stack2.pop().val);
-    }
-
-    return ans
- }
-
-
-var postorderTraversal = function(root) {
-    let res= postOrderTraversalUsingItrative(root);
+var postorderTraversal = function (root) {
     // let res= postOrderTraversalUsingRecursion(root);
+    // let res= postOrderTraversalUsingItrativeWithTwoStack(root);
+    let res = postOrderTraversalUsingItrativeWithOneStack(root);
     return res;
 };
 
 
 
-function postOrderTraversalUsingRecursion(root){
-    let ans=[];
-    function traverse(curr){
-        if(!curr) return;
+
+// Using Recurrsion... ...........................................
+function postOrderTraversalUsingRecursion(root) {
+    let ans = [];
+    function traverse(curr) {
+        if (!curr) return;
         traverse(curr.left)
         traverse(curr.right);
         ans.push(curr.val)
     }
     traverse(root);
+    return ans;
+}
+
+
+
+// Using Two Stack.. ...........................................
+function postOrderTraversalUsingItrativeWithTwoStack(root) {
+    if (!root) return [];
+
+    let stack1 = [root];
+    let stack2 = [];
+    while (stack1.length) {
+        let curr = stack1.pop();
+        stack2.push(curr);
+        curr.left && stack1.push(curr.left);
+        curr.right && stack1.push(curr.right);
+    }
+
+    let ans = [];
+
+    while (stack2.length) {
+        ans.push(stack2.pop().val);
+    }
+
+    return ans
+}
+
+
+
+
+// Using One Stack...........................................
+function postOrderTraversalUsingItrativeWithOneStack(root) {
+    let stack = [];
+    let curr = root;
+    let ans = [];
+    let lastVisitedNode=null;
+
+
+    while (curr || stack.length) {
+        // Push All the Values to stack till I reach leftmost bottom
+        while (curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+
+        // Peeking TOP Element from stack
+        let peekNode = stack[stack.length - 1];
+
+        // If right exists & it is not the Last Visited
+        if (peekNode.right && peekNode.right!==lastVisitedNode) {
+            curr = peekNode.right;
+        } else {
+            ans.push(peekNode.val);
+            lastVisitedNode=stack.pop();
+        }
+
+    }
     return ans;
 }
